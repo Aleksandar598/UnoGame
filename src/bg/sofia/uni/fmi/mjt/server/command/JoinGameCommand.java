6eac7c56591b1cd56a224e507d8da0697d4a6548
Aleksandar598 <alexanderangelov425@gmail.com>
2026-08-22 19:17:39 +0300

    Fixed typo

--- src/bg/sofia/uni/fmi/mjt/server/command/JoinGameCommand.java
+++ src/bg/sofia/uni/fmi/mjt/server/command/JoinGameCommand.java
@@ -30,7 +30,7 @@ public class JoinGameCommand extends AbstractCommand {
     private static final String MAX_PLAYER_COUNT = "Game you want to join is full";
     private static final String GAME_STARTED = "Game has already started";
     private static final String GAME_NOT_FOUND = "Game has not been found";
-    private static final String SUCCESS = " successfully joined game";
+    private static final String SUCCESS = "Successfully joined game";
     private static final String INVALID_ARGS = "Invalid arguments";
 
     public JoinGameCommand() {
@@ -52,7 +52,7 @@ public class JoinGameCommand extends AbstractCommand {
             parse(input, socket);
             Player player = userManager.bindSocketToPlayer(socket, displayName);
             gameManager.joinGame(gameId, player);
-            gameManager.notifyAllInAGame(player, player.getName() + SUCCESS );
+            gameManager.notifyAllInAGame(player, player.getName() + ' ' + SUCCESS );
 
         } catch (UserNotLoggedInException e) {
             ExceptionLogger.logException(e);
